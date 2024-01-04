833ffe9d93a7c754ff25bbbf013880aa20b8dc68
Angel-Ceballos <angel.ceballos.esp@gmail.com>
2024-01-04 13:58:44 -0600

    modified config.py and dataset.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ NUM_WORKERS = 8
 # CHECKPOINT_FILE = "best_model_ResNet18_TCD_128x128.pth"
 CHECKPOINT_FILE = "placeholder.pth"
 # FILE_NAME = "ResNet18_TCD_5KP_Climate_128x128x3"
-FILE_NAME = "CspResNeXt50_LR_HM"
+FILE_NAME = "Resnet18_car"
 PIN_MEMORY = True
 SAVE_MODEL = True
 LOAD_MODEL = True
@@ -25,110 +25,42 @@ CLASS_NUM = 5
 # Data augmentation for images
 train_transforms = A.Compose(
     [
-        #A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE),
         A.Rotate(limit=10, p=0.25),
         A.IAAAffine(shear=15, scale=1.0, mode="constant", p=0.2),
-        A.RandomBrightnessContrast(contrast_limit=0.5, brightness_limit=0.8, p=0.25),
+        A.RandomBrightnessContrast(contrast_limit=0.5, brightness_limit=0.5, p=0.25),
         A.OneOf([
             A.GaussNoise(p=0.8),
-            A.CLAHE(p=0.8),
             A.ImageCompression(p=0.8),
             A.RandomGamma(p=0.8),
-            A.Posterize(p=0.8),
-            A.Blur(p=0.8),
-        ], p=1.0),
-        A.OneOf([
-            A.GaussNoise(p=0.8),
-            A.CLAHE(p=0.8),
-            A.ImageCompression(p=0.8),
-            A.RandomGamma(p=0.8),
-            A.Posterize(p=0.8),
             A.Blur(p=0.8),
         ], p=1.0),
         A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=0, p=0.5),
-        A.CenterCrop(width=128, height=128),
+        A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]),
         ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
+    ]
 )
-
-train_ext_transforms =A.Compose([
-    A.IAAAffine(shear=15, scale=1.0, mode="constant", p=0.2),
-    A.RandomBrightness(limit=0.6, p=0.35),
-    A.OneOf([
-        A.GaussNoise(p=0.8),
-        A.CLAHE(p=0.8),
-        A.ImageCompression(p=0.8),
-        A.RandomGamma(p=0.8),
-        A.Posterize(p=0.8),
-        A.Blur(p=0.8),
-    ], p=1.0),
-    A.OneOf([
-        A.GaussNoise(p=0.8),
-        A.CLAHE(p=0.8),
-        A.ImageCompression(p=0.8),
-        A.RandomGamma(p=0.8),
-        A.Posterize(p=0.8),
-        A.Blur(p=0.8),
-    ], p=1.0),
-    A.OneOf([
-        A.RandomSnow(p=0.2),
-        A.RandomFog(p=0.2),
-        A.RandomRain(p=0.2),
-        A.HueSaturationValue(0.2),
-    ], p=0.5),
-    A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=0.05, p=0.8),
-    A.CenterCrop(width=128, height=128),
-    A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]),
-    ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False)
-)
-
 
 val_transforms = A.Compose(
     [
-        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=0, p=0.3),
-        A.CenterCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
+        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=0.1, p=0.3),
+        A.Resize(width=IMAGE_SIZE, height=IMAGE_SIZE),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]),
         ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
+    ]
 )
 
-val_ext_transforms = A.Compose(
-    [
-        A.CenterCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
-        A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]),
-        ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
-)
 
 test_transforms = A.Compose(
     [
         A.Resize(height=IMAGE_SIZE, width=IMAGE_SIZE),
-        A.Rotate(limit=10, border_mode=cv2.BORDER_CONSTANT, p=0.25),
-        A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=0, p=0.5, border_mode=cv2.BORDER_CONSTANT),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]),
         ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
-)
-
-test_ext_transforms = A.Compose(
-    [
-        A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=0, p=0.2),
-        A.CenterCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
-        A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]),
-        ToTensorV2(),
-    ], keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
+    ]
 )
--- dataset.py
+++ dataset.py
@@ -9,15 +9,17 @@ from torch.utils.data import DataLoader, Dataset
 from config import OG_SIZE, CLASS_NUM
 
 
-class CouplerKeypointDataset(Dataset):
-    def __init__(self, csv_file, train=True, transform=None):
+class CarDataset(Dataset):
+    def __init__(self, csv_file, train=True, debug=False, transform=None):
         super().__init__()
         random.seed(42)
         np.random.seed(42)
         self.data = pd.read_csv(csv_file)
-        self.category_names = ['center_x', 'center_y', 'left_x', 'left_y', 'top_x', 'top_y', 'right_x', 'right_y', 'buttom_x', 'buttom_y', 'confidence']
-        # self.category_names = ['center_x', 'center_y', 'left_x', 'left_y', 'top_x', 'top_y', 'right_x', 'right_y', 'buttom_x', 'buttom_y']
-        # self.category_names = ['left_x', 'left_y', 'top_x', 'top_y', 'right_x', 'right_y', 'buttom_x', 'buttom_y']
+        self.category_names = ['color', 'type', 'orientation']
+        self.colors = {'red':0, 'green':1, 'yellow':2, 'orange':3, 'blue':4, 'purple':5, 
+                       'black':6, 'white':7, 'brown':8, 'gray':9}
+        self.types = {'hatchback':0, 'sedan':1, 'minivan':2, 'van':3, 'suv':4, 'pickup':5, 'truck':6, 'bus':7}
+        self.orientations = {'front':0, 'back':1, 'fl':2, 'fr':3, 'bl':4, 'br':5, 'left':6, 'right':7}
         self.transform = transform
         self.train = train
 
@@ -26,49 +28,31 @@ class CouplerKeypointDataset(Dataset):
 
     def __getitem__(self, index):
         if self.train:
-            # class_visibility = self.data.iloc[index]['Class']
-            # src_dir = "medimages" if class_visibility else "negimages"
-            src_dir= "medimages"
-            src = self.data.iloc[index]['Source'].split("\\")[1:]
-            src.insert(0, '/media/angel/My Passport/THA')
-            src = "".join([i+"/" for i in src])
-            img_path = os.path.join(src, src_dir, self.data.iloc[index]['Image'])
+            src_dir= "/home/angel/computer_vision/car-grouping/subset"
+            img_path = os.path.join(src_dir, self.data.iloc[index]['file']+'.jpg')
             image = cv2.imread(img_path)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            w = self.data.iloc[index]['Width']//2
-            h = self.data.iloc[index]['Height']//2
-            m = OG_SIZE//2
-            # color = self.data.iloc[index]['Color']
-            # c = 0 if color == 'black' else 1
-            labels = np.array([m, m, m-w, m, m, m-h, m+w, m, m, m+h])
-            # labels = np.array([m-w, m, m, m-h, m+w, m, m, m+h])
-        # else:
-        #     img_path = os.path.join(self.data.iloc[index]['Source'], "tb_medimages", self.data.iloc[index]['Image'])
-        #     image = cv2.imread(img_path)
-        #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #     labels = np.zeros(CLASS_NUM*2)
-
-        labels = labels.reshape(CLASS_NUM, 2)
+            color = self.colors.get(self.data.iloc[index]['color'])
+            c_type = self.types.get(self.data.iloc[index]['type'])
+            orientation = self.orientations.get(self.data.iloc[index]['orientation'])
+            labels = np.array([color, c_type, orientation])
 
         if self.transform:
-            augmentations = self.transform(image=image, keypoints=labels)
+            augmentations = self.transform(image=image)
             image = augmentations["image"]
-            labels = augmentations["keypoints"]
-
-        # if not class_visibility:
-        #     labels = np.zeros(CLASS_NUM*2)
-
-        labels = np.array(labels).reshape(-1)
-        # labels = np.append(labels, class_visibility)
+        
 
         return image, labels.astype(np.float32)
 
 
 if __name__ == "__main__":
-    ds = CouplerKeypointDataset(csv_file=r"/media/angel/My Passport/THA/tcd_code/data/TCD_FULL_SNOWY_train.csv", train=True, transform=config.train_ext_transforms)
+    ds = CarDataset(csv_file="/home/angel/computer_vision/car-grouping/labels/train_data.csv", train=True, transform=config.train_transforms)
     loader = DataLoader(ds, batch_size=1, shuffle=True, num_workers=0)
     for idx, (x, y) in enumerate(loader):
-        plt.imshow(x[0][0].detach().cpu().numpy())
+        img = x[0][0].detach().cpu().numpy()
+        plt.imshow(img)
+        plt.axis("off")
+        print(y)
         #plt.plot(y[0][0].detach().cpu().numpy() + y[0][2].detach().cpu().numpy()//2, y[0][1].detach().cpu().numpy()+y[0][2].detach().cpu().numpy()//2, "go")
-        plt.plot(y[0][0:10:2].detach().cpu().numpy(), y[0][1:11:2].detach().cpu().numpy(), "go")
+        # plt.plot(y[0][0:10:2].detach().cpu().numpy(), y[0][1:11:2].detach().cpu().numpy(), "go")
         plt.show()
